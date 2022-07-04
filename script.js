2b86c9c125c708429b94dd4820ba37f17c6713e9
GOODFARMS-ENG <berteng90@gmail.com>
2022-07-04 16:36:48 +0800

    Fixed Dynamic Grid Logic

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let gridSize = 16;
+let gridLength = 16;
 
 
 function generateGrid() {
     const grid = document.getElementById('grid');
-    for (x = 1; x < gridSize; i++) {
+    for (x = 1; x < gridLength; x++) {
         const gridRow = document.createElement('div');
         gridRow.setAttribute('id', 'row');
-        gridRow.style.backgroundColor = 'white';
         for (y = 1; y <= 16; y++) {
             const gridColumn = document.createElement('div');
             gridColumn.setAttribute('id', 'column');
-            gridColumn.style.width = '50px';
-            gridColumn.style.height = '50px';
+            gridColumn.style.width = '38px';
+            gridColumn.style.height = '100%';
             gridColumn.style.backgroundColor = '#36454F';
             gridRow.appendChild(gridColumn);
         }
