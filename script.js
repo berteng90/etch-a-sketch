3376d35841007bb6d772ae5cc49eef3ef0c9fa66
GOODFARMS-ENG <berteng90@gmail.com>
2022-07-04 16:50:51 +0800

    Simple UI tweaks

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function generateGrid() {
         for (y = 1; y <= 16; y++) {
             const gridColumn = document.createElement('div');
             gridColumn.setAttribute('id', 'column');
-            gridColumn.style.width = '38px';
+            gridColumn.style.width = '50px';
             gridColumn.style.height = '100%';
             gridColumn.style.backgroundColor = '#36454F';
             gridRow.appendChild(gridColumn);
