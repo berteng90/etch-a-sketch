62e5c21de3b54e9daf38c1af6e46f8afa4c5a810
Englebert Y. Garay <berteng90@gmail.com>
2022-07-05 04:57:28 +0800

    Added final touches to UI

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let gridLength = 16;
 
 function generateGrid() {
     const grid = document.getElementById('grid');
-    for (x = 1; x < gridLength; x++) {
+    for (x = 1; x <= gridLength; x++) {
         const gridRow = document.createElement('div');
         gridRow.setAttribute('id', 'row');
-        for (y = 1; y <= 16; y++) {
+        for (y = 1; y <= gridLength; y++) {
             const gridColumn = document.createElement('div');
             gridColumn.setAttribute('id', 'column');
-            gridColumn.style.width = '50px';
-            gridColumn.style.height = '100%';
-            gridColumn.style.backgroundColor = '#36454F';
+            gridColumn.style.width = 'auto';
+            gridColumn.style.height = 'auto';
+            gridColumn.style.backgroundColor = '#FAF9F6';
             gridRow.appendChild(gridColumn);
         }
         grid.appendChild(gridRow);
