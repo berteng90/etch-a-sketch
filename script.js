2af7866164e9b79e78f9cad0b809493b59f946be
GOODFARMS-ENG <berteng90@gmail.com>
2022-07-05 16:29:14 +0800

    Mouse move function added

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let gridLength = 16;
 let color = '#343434';
 let mode = 0;
-let randomColor = '#000000';
+let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let mouseStatus = false;
 
 function updateMode() {
-    let button = document.getElementById('color');
+    let button = document.querySelector('#color');
     if (mode === 0) {
-        console.log('test');
         mode = 1
+
     } else if (mode === 1) {
         mode = 0;
         button.style.transform = 1;
@@ -25,7 +26,8 @@ function clearGrid() {
 }
 
 function randomizeColor() {
-    randomColor = '#000000';
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor);
 }
 
 function updateColor(clr) {
@@ -54,21 +56,27 @@ function generateGrid() {
             gridColumn.style.backgroundColor = '#FAF9F6';
             gridRow.appendChild(gridColumn);
             staticGrid(gridColumn);
-            grid.appendChild(gridRow);
         }
+        grid.appendChild(gridRow);
     }
 }
 
 function staticGrid(column) {
-    column.addEventListener('click', function () {
-        if (mode === 0) {
-            column.style.backgroundColor = color;
-        } else if (mode === 1) {
-            randomizeColor();
-            column.style.backgroundColor = randomColor;
-        }
+    if (mouseStatus === true) {
+        column.addEventListener('mousemove', () => {
+            column.style.backgroundColor = '#343434';
+        })
+    }
 
-    })
 }
+
+function mouseReset() {
+    mouseStatus = false;
+}
+
+
+
+
+
 
 
