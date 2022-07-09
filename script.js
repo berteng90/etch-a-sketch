cf3000e79fc982be72dfe57b24b7381a46d7eea6
Englebert Y. Garay <berteng90@gmail.com>
2022-07-09 18:58:08 +0800

    Completed Overall Logic and Function for TheOdinProject Etch a Sketch

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-let gridLength = 16;
 let color = '#343434';
-let mode = 0;
-let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let mode = 'static';
+let eraser=false;
 let mouseStatus = false;
-let gridSize=[3,9,16,36,64];
+let gridSize=[3,9,16,32,64];
 
+
+//Selects Color mode
 function updateMode() {
     let button = document.querySelector('#color');
-    if (mode === 0) {
-        mode = 1
+        if (mode === 'static') {
+            mode = 'random'
+            button.style.backgroundColor='#28282B'
+            button.style.color='#F9F6EE'          
+    
+        } else if (mode === 'random') {
+            mode = 'static';
+            button.style.backgroundColor=''
+            button.style.color='#28282B'  
+        }     
+}
 
-    } else if (mode === 1) {
-        mode = 0;
-        button.style.transform = 1;
-    } else {
-
+//Enable/Disable Eraser
+function eraseGrid(){
+    let button = document.querySelector('#eraser');
+    if (eraser===false){
+        eraser = true
+        button.style.backgroundColor='#28282B'
+        button.style.color='#F9F6EE'
+        color='#FAF9F6'
+    }
+    else{
+        eraser=false
+        button.style.backgroundColor=''
+        button.style.color='#28282B'
     }
 }
 
+//clear Current Grid
 function clearGrid() {
     let reset = document.querySelectorAll('#column');
     reset.forEach(div => {
@@ -26,28 +45,34 @@ function clearGrid() {
 
 }
 
+//randomize Color when Randomize option is selected
 function randomizeColor() {
     let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    console.log(randomColor);
+    color=randomColor
 }
 
 function updateColor(clr) {
     color = clr;
 }
 
+//update Grid Size
 function updateGrid(size){
-    let gridChange=document.getElementById('gridbar')
-    gridLength=gridSize[size];  
-    console.log(gridLength)
-    gridChange.textContent=`${gridLength} X ${gridLength}`
+    let gridChange=document.getElementById('gridbar') 
+    gridChange.textContent=`${gridSize[size]} X ${gridSize[size]}`
     clearGrid()
-    generateGrid(gridLength)
+    generateGrid(gridSize[size])  
 }
 
-
-function generateGrid() {
+//populate Grid with dynamic Rows & Columns
+function generateGrid(gridLength) {
+    if (gridLength===undefined){
+        gridLength=16
+    }    
     const grid = document.getElementById('grid');
+    const del=document.querySelectorAll('div #row')
+    del.forEach((div)=>div.remove())
     for (x = 1; x <=gridLength; x++) {
+        console.log(gridLength) 
         const gridRow = document.createElement('div');
         gridRow.setAttribute('id', 'row');
         for (y = 1; y <= gridLength; y++) {
@@ -57,7 +82,7 @@ function generateGrid() {
             gridColumn.style.height = 'auto';
             gridColumn.style.backgroundColor = '#FAF9F6';
             gridRow.appendChild(gridColumn);
-            staticGrid(gridColumn);   
+            staticGrid(gridColumn);  
         }
         grid.appendChild(gridRow);
     }
@@ -65,27 +90,46 @@ function generateGrid() {
 
 
 
+//Update focused DIV's Background
 function staticGrid(column) {
-    
-        column.addEventListener('mousemove', () => {    
-            if (mouseStatus===true){
-            column.style.backgroundColor='#28282B'
+    let picker=document.getElementById('picker').value
+    column.addEventListener('mousedown', () => {
+        if(mode==='random' && eraser!==true){
+            randomizeColor()
+            column.style.backgroundColor=color
+        }else if(mode==='static' && eraser!==true){
+            let picker=document.getElementById('picker').value
+            column.style.backgroundColor=picker
+        }else if(mode==='static' && eraser===true){
+            column.style.backgroundColor=color
         }
-        })  
-    }
-          
-   
-               
-
+    })
+        column.addEventListener('mouseenter', () => {    
+            if (mouseStatus===true){
+                if(mode==='random' && eraser!==true){
+                    randomizeColor()
+                    column.style.backgroundColor=color
+                }else if(mode==='static' && eraser!==true){
+                    let picker=document.getElementById('picker').value
+                    column.style.backgroundColor=picker
+                }else if(mode==='static' && eraser===true){
+                    column.style.backgroundColor=color
+                }else{
+                    if(confirm('You Cant Enable Eraser & Randomizer at the same time!')===true){
+                        updateMode()
+                    }
+                }
+            
+        }
+        })
+    }      
+            
 function mouseDown(){ 
     mouseStatus=true
-    console.log(mouseStatus)
 }
-
 
 function mouseUp(){   
     mouseStatus=false
-    console.log(mouseStatus)
 }
 
 
